9f298e632ae3a55769f5147f37750f44837a188d
Martin Kavík <martin@kavik.cz>
2024-10-24 15:19:02 +0200

    update components code

--- src/components/counter/src/lib.rs
+++ src/components/counter/src/lib.rs
@@ -44,26 +44,26 @@ mod task {
     use std::future::Future;
     use std::env;
 
-    // use crate::bindings::golem::component_runtime_loop_stub::stub_runtime_loop::*;
+    use crate::bindings::golem::component_runtime_loop_stub::stub_runtime_loop::*;
 
-    // thread_local! {
-    //     static TASK_POOL: RefCell<LocalPool> = Default::default();
-    //     static TASK_SPAWNER: LocalSpawner = TASK_POOL.with_borrow(|pool| {
-    //         let component_id = env::var("RUNTIME_LOOP_COMPONENT_ID").expect("RUNTIME_LOOP_COMPONENT_ID not set");
-    //         let runtime_loop_worker = ApiRuntimeLoop::new(&GolemRpcUri {
-    //             value: format!("urn:worker:{component_id}/runtime_loop_1")
-    //         });
-    //         println!("Calling loop worker to start the loop...");
-    //         runtime_loop_worker.start_loop();
-    //         pool.spawner()
-    //     });
-    // }
+    thread_local! {
+        static TASK_POOL: RefCell<LocalPool> = Default::default();
+        static TASK_SPAWNER: LocalSpawner = TASK_POOL.with_borrow(|pool| {
+            let component_id = env::var("RUNTIME_LOOP_COMPONENT_ID").expect("RUNTIME_LOOP_COMPONENT_ID not set");
+            let runtime_loop_worker = ApiRuntimeLoop::new(&GolemRpcUri {
+                value: format!("urn:worker:{component_id}/runtime_loop_1")
+            });
+            println!("Calling loop worker to start the loop...");
+            runtime_loop_worker.start_loop();
+            pool.spawner()
+        });
+    }
 
     pub fn spawn(future: impl Future<Output = ()> + 'static) {
-        // TASK_SPAWNER.with(move |spawner| spawner.spawn_local_obj(Box::pin(future).into()).unwrap())
+        TASK_SPAWNER.with(move |spawner| spawner.spawn_local_obj(Box::pin(future).into()).unwrap())
     }
 
     pub fn run_all() {
-        // TASK_POOL.with_borrow_mut(|pool| pool.run_until_stalled())
+        TASK_POOL.with_borrow_mut(|pool| pool.run_until_stalled())
     }
 }
--- src/components/runtime_loop/src/lib.rs
+++ src/components/runtime_loop/src/lib.rs
@@ -1,25 +1,28 @@
 mod bindings;
 
 use crate::bindings::exports::golem::component_runtime_loop::api_runtime_loop::*;
-// use crate::bindings::golem::component_golem_async_stub::stub_golem_async::*;
-use golem_rust::bindings::wasi;
+use crate::bindings::golem::component_counter_stub::stub_counter::*;
+use golem_rust::bindings::wasi::clocks::monotonic_clock::{self, Duration};
 use std::env;
 
 struct Component;
 
+const MILLISECOND: Duration =  1_000_000;
+const WAIT_TIME: Duration = 2_000 * MILLISECOND;
+
 impl Guest for Component {
     fn start_loop() {
-        // let component_id = env::var("GOLEM_ASYNC_COMPONENT_ID").expect("GOLEM_ASYNC_COMPONENT_ID not set");
-        // let golem_async_worker = ApiGolemAsync::new(&GolemRpcUri {
-        //     value: format!("urn:worker:{component_id}/golem_async_1")
-        // });
-        // println!("Loop starting!");
-        // loop {
-        //     wasi::clocks::monotonic_clock::subscribe_duration(2_000_000_000).block();
-        //     println!("calling 'run_all_tasks()' ...");
-        //     golem_async_worker.blocking_run_all_tasks();
-        //     println!("'run_all_tasks()' called");
-        // }
+        let component_id = env::var("COUNTER_COMPONENT_ID").expect("COUNTER_COMPONENT_ID not set");
+        let counter_worker = ApiCounter::new(&GolemRpcUri {
+            value: format!("urn:worker:{component_id}/counter_1")
+        });
+        println!("Loop starting!");
+        loop {
+            monotonic_clock::subscribe_duration(WAIT_TIME).block();
+            println!("calling 'run_all_tasks()' ...");
+            counter_worker.blocking_run_all_tasks();
+            println!("'run_all_tasks()' called");
+        }
     }
 }
 
